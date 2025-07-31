e6ca808953753384a7916e4c28789b38250c363e
judryp25s-beep <judry.p25s@gmail.com>
2025-07-31 21:10:07 +0100

    Update application.py

--- api/application.py
+++ api/application.py
@@ -1,11 +1,6 @@
 from flask import Flask, render_template
 
-app = Flask(__name__)
-
-@app.route('/Zo')
+app = Flask(__name__, template_folder='../templates')
+@app.route('/')
 def home():
     return render_template('kitten.html')
-
-if __name__ == '__main__':
-    # Accès depuis un autre appareil en réseau local
-    app.run(debug=True, host='0.0.0.0', port=5000)
