071f5699bdb9650ba68118e25ed90c26e0ba23cc
judryp25s-beep <judry.p25s@gmail.com>
2025-08-02 13:27:15 +0300

    Update application.py

--- api/application.py
+++ api/application.py
@@ -1,6 +1,28 @@
-from flask import Flask, render_template
+from flask import Flask, jsonify, render_template
+import requests
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+API_KEY_CAT = os.getenv("CAT_API_KEY")
+API_KEY_DOG = os.getenv("DOG_API_KEY")
+API_URL_DOG = "https://api.thedogapi.com/v1/images/search?limit=10"
 
 app = Flask(__name__, template_folder='../templates')
+
 @app.route('/')
-def home():
-    return render_template('kitten.html')
+def index():
+    return render_template('main.html')
+
+@app.route('/dogs')
+def get_dogs():
+    headers = {"x-api-key": API_KEY_DOG}
+    response = requests.get(API_URL_DOG, headers=headers)
+    return jsonify(response.json())
+
+
+@app.route('/cats')
+def get_dogs():
+    headers = {"x-api-key": API_KEY_CAT}
+    response = requests.get(API_URL_CAT, headers=headers)
+    return jsonify(response.json())
